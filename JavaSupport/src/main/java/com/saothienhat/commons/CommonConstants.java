c939f45dfc42c7fcc98c65fac13a691c8de11430
Sao Thien Hat <saothienhat@yahoo.com>
2015-09-03 11:10:50 +0700

    Added common constants

---
+++ JavaSupport/src/main/java/com/saothienhat/commons/CommonConstants.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015 Trinh Thanh Binh. All rights reserved.
+ */
+package com.saothienhat.commons;
+
+/**
+ * @author binhtt
+ *
+ */
+public final class CommonConstants{
+    /*
+     * Characters
+     */
+    public static final String CHARACTER_STAR = "*";
+    public static final String CHARACTER_SLASH = "/";
+    public static final String CHARACTER_QUOTATION_MARK = "\"";
+    public static final String CHARACTER_HASHTAG = "#";
+    public static final String CHARACTER_DOLLAR_SIGN = "$";
+    public static final String CHARACTER_PERCENT_SIGN = "%";
+    public static final String CHARACTER_AMPERSAND = "&";
+    public static final String CHARACTER_LEFT_PARENTHESIS = "(";
+    public static final String CHARACTER_RIGHT_PARENTHESIS = ")";
+    public static final String CHARACTER_COMMA = ",";
+    public static final String CHARACTER_PLUS = "+";
+    public static final String CHARACTER_MINUS = "-";
+    public static final String CHARACTER_EXCLAMATION_MARK = "!";
+    public static final String CHARACTER_SPACE = " ";
+    public static final String CHARACTER_AT_SIGN = "@";
+    public static final String CHARACTER_QUESTION_MARK = "?";
+    public static final String CHARACTER_GREATERTHAN_SIGN = ">";
+    public static final String CHARACTER_EQUAL_SIGN = "=";
+    public static final String CHARACTER_LESSTHAN_SIGN = "<";
+    public static final String CHARACTER_SEMICOLON = ";";
+    public static final String CHARACTER_COLON = ":";
+    public static final String CHARACTER_BACKSLASH = "\\";
+    public static final String CHARACTER_LEFT_SQUARE_BRACKET = "[";
+    public static final String CHARACTER_RIGHT_SQUARE_BRACKET = "]";
+    public static final String CHARACTER_CIRCUMFLEX_ACCENT = "^";
+    public static final String CHARACTER_LOW_LINE = "_";
+    public static final String CHARACTER_LEFT_CURLY_BRACKET = "{";
+    public static final String CHARACTER_RIGHT_CURLY_BRACKET = "}";
+    public static final String CHARACTER_TILDE = "~";
+    public static final String CHARACTER_VERTICAL_BAR = "|";
+    
+
+}
---
+++ JavaSupport/src/main/java/com/saothienhat/commons/DBConstants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Trinh Thanh Binh. All rights reserved.
+ */
+package com.saothienhat.commons;
+
+/**
+ * @author binhtt
+ *
+ */
+public final class DBConstants {
+    /*
+     * Tables
+     */
+    public static final String TABLE_TEST = "TEST";
+    /*
+     * Columns
+     */
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_NAME = "NAME";
+    /*
+     * Queries
+     */
+    public static final String QUERY_SELECT_ALL = "SELECT * FROM ";
+
+}
