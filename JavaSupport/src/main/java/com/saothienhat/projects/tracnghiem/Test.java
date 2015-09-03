eef709d55e6c8fcf793f6d7afaea616a33439305
Sao Thien Hat <saothienhat@yahoo.com>
2015-09-03 10:13:31 +0700

    Add TracNghiem project
    Add TracNghiem project

---
+++ JavaSupport/src/main/java/com/saothienhat/projects/tracnghiem/Test.java
@@ -0,0 +1,40 @@
+package com.saothienhat.projects.tracnghiem;
+
+public class Test {
+	private Question question;
+	private Answer answer;
+	
+	/**
+	 * @return the question
+	 */
+	public Question getQuestion() {
+		return question;
+	}
+	
+	/**
+	 * @param question the question to set
+	 */
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+	
+	/**
+	 */
+	public Test() {
+	}
+
+	/**
+	 * @return the answer
+	 */
+	public Answer getAnswer() {
+		return answer;
+	}
+	
+	/**
+	 * @param answer the answer to set
+	 */
+	public void setAnswer(Answer answer) {
+		this.answer = answer;
+	}
+
+}
